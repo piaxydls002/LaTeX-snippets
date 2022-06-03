8630d7aa4454207b9c25abe2e917736582d5353d
piaxydls_pen_s <piaxydls000@gmail.com>
2022-06-03 16:11:17 +0900

    .

--- pythonx/util.py
+++ pythonx/util.py
@@ -1,3 +1,16 @@
+import re, os, vim, string, random
+
+# Contexts
+def math():
+	return vim.eval('vimtex#syntax#in_mathzone()') == '1'
+
+def comment():
+	return vim.eval('vimtex#syntax#in_comment()') == '1'
+
+def env(name):
+	[x,y] = vim.eval("vimtex#env#is_inside('" + name + "')")
+	return x != '0' and y != '0'
+
 # Utils
 
 def complete(t, opts):
